ccacb2ce0e7388729c3d6cf0c163a679bc196504
Manuel Caicedo <manuelcaicedo52@gmail.com>
2023-10-01 10:59:49 -0500

    create form use the Material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import  Container  from '@mui/material/Container';
 import FormSingUp from './components/FormSignUp';
+import  Typography  from '@mui/material/Typography';
 
 
 function App() {
   return (
-    <>
-      <h1>Formulario Registro</h1>
+    <Container component="section" maxWidth="sm">
+      <Typography variant='h3' align='center'>Formulario Registro</Typography>
       <FormSingUp/>
-    </>
+    </Container>
   );
 }
 
--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,17 +1,30 @@
-import { Button } from "@mui/material"
+import { Button,TextField, Switch, FormGroup, FormControlLabel } from "@mui/material"
 
 function FormSingUp (){
     return (<form>
-            <label>Nombre</label>
-            <input type="text"/>
-            <label>Apellidos</label>
-            <input type="text"/>
-            <label>Correo electronico</label>
-            <input type="text"/>
-            <label>Promociones</label>
-            <input type="checkbox"/>
-            <label>Novedades</label>
-            <input type="checkbox"/>
+            <TextField 
+                id="name" 
+                label="Nombre" 
+                variant="outlined"
+                fullWidth
+                margin="normal"/>
+            <TextField 
+                id="lastname" 
+                label="Apellidos" 
+                variant="outlined"
+                fullWidth
+                margin="normal"/>           
+            <TextField 
+                id="email" 
+                label="Correo" 
+                variant="outlined"
+                fullWidth
+                margin="normal"/>
+            <FormGroup>
+                <FormControlLabel control={<Switch defaultChecked/>} label="Promociones"/> 
+                <FormControlLabel control={<Switch defaultChecked/>} label="Novedades"/>   
+            </FormGroup>    
+            
             <Button variant="contained">Registrarse</Button>
         </form> 
     )  
