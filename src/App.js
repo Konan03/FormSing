144930c9b4b7eb6f6eda59cfc423c883cb4a36e4
Manuel Caicedo <manuelcaicedo52@gmail.com>
2023-10-01 12:21:21 -0500

    complete form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import  Container  from '@mui/material/Container';
 import FormSingUp from './components/FormSignUp';
 import  Typography  from '@mui/material/Typography';
 
+const handleSubmit = (valores) => {
+  console.log("APPJS: ", valores)
+}
 
 function App() {
   return (
     <Container component="section" maxWidth="sm">
-      <Typography variant='h3' align='center'>Formulario Registro</Typography>
-      <FormSingUp/>
+      <Typography variant='h3' align='center' >Formulario Registro</Typography>
+      <FormSingUp handleSubmit={handleSubmit}/>
     </Container>
   );
 }
--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,31 +1,89 @@
 import { Button,TextField, Switch, FormGroup, FormControlLabel } from "@mui/material"
+import {  useState } from "react"
 
-function FormSingUp (){
-    return (<form>
+function FormSingUp ({handleSubmit}){
+    const [name, setName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [prom, setProm] = useState(true)
+    const [nov, setNov] = useState(false)
+    const [errors, setErrors] = useState({
+        name: {
+            error: false,
+            message: ""
+        }
+    })
+
+    function validarNombre(nombre){
+        if(nombre.length >= 3){
+            return {name:{ error: false, message: ""}}
+        }else{
+            return {name:{ error: true, message: "Deben ser almenos 3 caracteres"}}
+        }
+    }
+
+    
+   
+    return (<form onSubmit={(e) => {
+            e.preventDefault()
+            handleSubmit({
+                name,
+                lastName,
+                email,
+                prom,
+                nov
+            })
+        }}>
             <TextField 
                 id="name" 
                 label="Nombre" 
                 variant="outlined"
                 fullWidth
-                margin="normal"/>
+                margin="normal"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                error={errors.name.error}
+                helperText={errors.name.error ? errors.name.message : ""}
+                onBlur={(e) => {
+                    setErrors(validarNombre(e.target.value))
+                }}
+            />
             <TextField 
                 id="lastname" 
                 label="Apellidos" 
                 variant="outlined"
-                fullWidth
-                margin="normal"/>           
+                fullWidth   
+                margin="normal"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                
+            />           
             <TextField 
                 id="email" 
                 label="Correo" 
                 variant="outlined"
                 fullWidth
-                margin="normal"/>
+                margin="normal"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
             <FormGroup>
-                <FormControlLabel control={<Switch defaultChecked/>} label="Promociones"/> 
-                <FormControlLabel control={<Switch defaultChecked/>} label="Novedades"/>   
+                <FormControlLabel 
+                    control={
+                        <Switch
+                            checked={prom} 
+                            onChange={(e) => setProm(e.target.checked)}/>} 
+                    label="Promociones"/> 
+
+                <FormControlLabel 
+                    control={   
+                        <Switch 
+                        checked={nov}
+                        onChange={(e) => setNov(e.target.checked)}/>}
+                    label="Novedades"/>   
             </FormGroup>    
             
-            <Button variant="contained">Registrarse</Button>
+            <Button variant="contained" type="submit">Registrarse</Button>
         </form> 
     )  
 }
